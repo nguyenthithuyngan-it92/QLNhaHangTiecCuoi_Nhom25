82d167466f72b57912474e7989a110713ca6ad45
nttn <1951052129ngan@ou.edu.vn>
2022-10-24 15:48:37 +0700

    bao cao

--- QLNhaHangTiecCuoi/src/main/webapp/resources/js/booking.js
+++ QLNhaHangTiecCuoi/src/main/webapp/resources/js/booking.js
@@ -5,7 +5,7 @@
  */
 
 
-/* global fetch, parseFloat */
+/* global fetch, parseFloat, moment */
 
 const isValidPhone = phone =>
     /(([03+[2-9]|05+[6|8|9]|07+[0|6|7|8|9]|08+[1-9]|09+[1-4|6-9]]){3})+[0-9]{7}\b/g.test(phone);
